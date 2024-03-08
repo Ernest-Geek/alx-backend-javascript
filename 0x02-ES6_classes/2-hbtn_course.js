ad53f821db617b524e90568b5081fe54abc2293b
Ernest-Geek <ernestampene1@gmail.com>
2024-03-08 00:03:14 +0000

    update

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') this._name = name;
+    else throw TypeError('Name must be a string');
+    if (typeof (length) === 'number') this._length = length;
+    else throw TypeError('Length must be a number');
+    if (Array.isArray(students)) {
+      students.forEach((student) => {
+        if (typeof student !== 'string') throw TypeError('student must be a String');
+      });
+      this._students = students;
+    } else throw TypeError('Students must be an array');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') this._name = name;
+    else throw TypeError('Name must be a string');
+  }
+
+  set length(length) {
+    if (typeof (length) === 'number') this._length = length;
+    else throw TypeError('Length must be a number');
+  }
+
+  set students(students) {
+    if (Array.isArray(students)) {
+      students.forEach((student) => {
+        if (typeof student !== 'string') throw TypeError('student must be an String');
+      });
+      this._students = students;
+    } else throw TypeError('Students must be an array');
+  }
+}
